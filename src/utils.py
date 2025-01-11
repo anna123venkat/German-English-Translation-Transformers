451a8fecafe6e1e08b1a8ccb0a2f1f0925ebe9e1
anna123venkat <113528514+anna123venkat@users.noreply.github.com>
2025-01-11 14:13:23 +0530

    Create utils.py

---
+++ src/utils.py
@@ -0,0 +1,102 @@
+import spacy
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+from torchtext.data.metrics import bleu_score
+
+spacy_de = spacy.load('de_core_news_sm')
+spacy_en = spacy.load('en_core_web_sm')
+
+
+def tokenize_de(text):
+    """
+    Tokenizes German text from a string into a list of strings.
+    """
+    return [tok.text for tok in spacy_de.tokenizer(text)]
+
+
+def tokenize_en(text):
+    """
+    Tokenizes English text from a string into a list of strings.
+    """
+    return [tok.text for tok in spacy_en.tokenizer(text)]
+
+
+def translate_sentence(sentence, src_field, trg_field, model, device, max_len=50):
+    model.eval()
+
+    if isinstance(sentence, str):
+        tokens = [token.text.lower() for token in spacy_de(sentence)]
+    else:
+        tokens = [token.lower() for token in sentence]
+
+    tokens = [src_field.init_token] + tokens + [src_field.eos_token]
+
+    src_indexes = [src_field.vocab.stoi[token] for token in tokens]
+
+    src_tensor = torch.LongTensor(src_indexes).unsqueeze(0).to(device)
+
+    src_mask = model.make_src_mask(src_tensor)
+
+    with torch.no_grad():
+        enc_src = model.encoder(src_tensor, src_mask)
+
+    trg_indexes = [trg_field.vocab.stoi[trg_field.init_token]]
+
+    for _ in range(max_len):
+        trg_tensor = torch.LongTensor(trg_indexes).unsqueeze(0).to(device)
+        trg_mask = model.make_trg_mask(trg_tensor)
+
+        with torch.no_grad():
+            output, attention = model.decoder(trg_tensor, enc_src, trg_mask, src_mask)
+
+        pred_token = output.argmax(2)[:, -1].item()
+
+        trg_indexes.append(pred_token)
+
+        if pred_token == trg_field.vocab.stoi[trg_field.eos_token]:
+            break
+
+    trg_tokens = [trg_field.vocab.itos[i] for i in trg_indexes]
+
+    return trg_tokens[1:], attention
+
+
+def display_attention(sentence, translation, attention, n_heads=8, n_rows=4, n_cols=2):
+    assert n_rows * n_cols == n_heads
+
+    fig = plt.figure(figsize=(15, 25))
+
+    for i in range(n_heads):
+        ax = fig.add_subplot(n_rows, n_cols, i + 1)
+
+        _attention = attention.squeeze(0)[i].cpu().detach().numpy()
+
+        cax = ax.matshow(_attention, cmap='bone')
+
+        ax.tick_params(labelsize=12)
+        ax.set_xticklabels([''] + ['<sos>'] + [t.lower() for t in sentence] + ['<eos>'], rotation=45)
+        ax.set_yticklabels([''] + translation)
+
+        ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
+        ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
+
+    plt.show()
+    plt.close()
+
+
+def calculate_bleu(data, src_field, trg_field, model, device, max_len=50):
+    trgs = []
+    pred_trgs = []
+
+    for datum in data:
+        src = vars(datum)['src']
+        trg = vars(datum)['trg']
+
+        pred_trg, _ = translate_sentence(src, src_field, trg_field, model, device, max_len)
+
+        pred_trg = pred_trg[:-1]  # Remove <eos> token
+
+        pred_trgs.append(pred_trg)
+        trgs.append([trg])
+
+    return bleu_score(pred_trgs, trgs)
